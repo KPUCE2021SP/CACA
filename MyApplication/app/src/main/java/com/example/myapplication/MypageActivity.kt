2f0375c9b075d713ff89fb5f00230139990fe365
unknown <trade741@naver.com>
2021-07-30 16:20:29 +0900

    mypage xml

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MypageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MypageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mypage_activity)
+    }
+}
