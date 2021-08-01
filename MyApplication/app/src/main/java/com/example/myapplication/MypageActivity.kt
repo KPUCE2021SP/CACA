cfb28faa8f1311eded1ebae311c5c94b0abeaaf2
pjy7814 <pjy7814@naver.com>
2021-08-02 01:28:42 +0900

    Mypage connect to authentication complete!

--- MyApplication/app/src/main/java/com/example/myapplication/MypageActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MypageActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.mypage_activity.*
 
 class MypageActivity : AppCompatActivity() {
@@ -20,6 +24,20 @@ class MypageActivity : AppCompatActivity() {
         setContentView(R.layout.mypage_activity)
 
         textViewName.setText(uid)
+
+        val db : FirebaseFirestore = Firebase.firestore
+
+        val city = hashMapOf(
+            "name" to "Los Angeles",
+            "state" to "CA",
+            "country" to "USA"
+        )
+
+        db.collection("Member").document("ID_MEMBER")
+            .update("city", true)
+//            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+//            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
     }
 
 }
