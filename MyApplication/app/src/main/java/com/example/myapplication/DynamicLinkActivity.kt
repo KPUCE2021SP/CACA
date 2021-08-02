2fff5eca5d182ee97be80c9b5e344e556eea014f
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-03 01:21:55 +0900

    Dynamic link *5 error

--- MyApplication/app/src/main/java/com/example/myapplication/DynamicLinkActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/DynamicLinkActivity.kt
@@ -22,22 +22,23 @@ class DynamicLinkActivity : AppCompatActivity() {
         // intent 넘겨서 들어왔을 경우 랜덤 코드 생성
         var characterTable = arrayOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-            "1", "2", "3", "4", "5", "6", "7", "8", "9") // 랜덤 코드 배열
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0") // 랜덤 코드 배열
 
         var code : String = ""
-        for (i in 0..8) { // 랜덤 코드 8자리 생성
+        for (i in 0..4) { // 랜덤 코드 5자리 생성
             val random = Random()
             val num = random.nextInt(characterTable.size)
             code += characterTable[num]
         }
 
-        inviteCodeView.setText(code) // 8자리 코드 랜덤 생성해서 set
+        inviteCodeView.setText(code) // 5자리 코드 랜덤 생성해서 set
 
 
 
         // 링크 타고 들어왔을 경우
         if (intent.action == Intent.ACTION_VIEW) { // 링크 타고 들어왔을 때 수행하는 코드
             val value1 = intent.data?.getQueryParameter("key") // link의 key 값을 value로 받는다.
+            Toast.makeText(applicationContext, "key = $value1", Toast.LENGTH_LONG).show()
             inviteCodeView.setText(value1)
         }
 
@@ -50,6 +51,7 @@ class DynamicLinkActivity : AppCompatActivity() {
         }
 
         var link = "https://familyship.page.link/DynamicLinkActivity?key=" + inviteCodeView.text.toString()
+        // https://familyship.page.link/DynamicLinkActivity?key=choi
 
 
         shareBtn.setOnClickListener(){ // sharebutton 누르면 링크 자체 복사
