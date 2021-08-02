8f7d46d84b9234d238cd46d9c75c1674d746a2db
Jimin731 <petitejini@naver.com>
2021-08-03 00:40:18 +0900

    inviteCode xml

---
+++ MyApplication/app/src/main/java/com/example/myapplication/inviteCode.kt
@@ -0,0 +1,11 @@
+package com.example.caca_xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inviteCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invite_code)
+    }
+}
