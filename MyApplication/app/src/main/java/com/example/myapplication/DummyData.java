f555cbd6515dc74176317c7afdf6617ac4ae2c05
Jimin731 <petitejini@naver.com>
2021-08-02 22:08:20 +0900

    mypage(ing)

---
+++ MyApplication/app/src/main/java/com/example/myapplication/DummyData.java
@@ -0,0 +1,12 @@
+package com.example.caca_xml;
+
+public class DummyData {
+    public static String sDummyData =
+            "1. 약이름1.\n" +
+                    "2. 약이름2. \n" +
+                    "3. 약이름\n" +
+                    "4. 약이름.\n" +
+                    "5. 약이름.\n" +
+                    "6. 약이름1\n" +
+                    "7. 약이름1.\n";
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MypageActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MypageActivity.kt
@@ -1,14 +1,21 @@
-package com.example.myapplication
+package com.example.caca_xml
 
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.firestore.FirebaseFirestore
+//import com.google.firebase.firestore.ktx.firestore
+//import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.mypage_activity.*
+import java.util.regex.Pattern
 
 class MypageActivity : AppCompatActivity() {
 
@@ -22,6 +29,8 @@ class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mypage_activity)
+
+        setContent(ll_contain, DummyData.sDummyData);
 
         textViewName.setText(uid)
 
@@ -37,6 +46,59 @@ class MypageActivity : AppCompatActivity() {
 
         db.collection("Member").document(uid).set(human)
 
+    }
+
+    private fun setContent(layout: LinearLayout, content: String) {
+
+        if (!TextUtils.isEmpty(content)) {
+            val splitContent = content.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val splitNumber = ArrayList<String>()
+            val splitText = ArrayList<String>()
+
+            // Content Text
+            val mVContentView = arrayOfNulls<View>(splitContent.size)
+            val mTvContentNumber = arrayOfNulls<TextView>(splitContent.size)
+            val mTvContentText = arrayOfNulls<TextView>(splitContent.size)
+
+            for (splitIdx in splitContent.indices) {
+                if (TextUtils.isEmpty(splitContent[splitIdx])) {
+                    splitNumber.add("")
+                    splitText.add("")
+
+                } else if (Pattern.matches("^[0-9]+$", splitContent[splitIdx].substring(0, 1))) {
+                    splitNumber.add(splitContent[splitIdx].substring(0, 2))
+                    splitText.add(splitContent[splitIdx].substring(2).trim { it <= ' ' })
+
+                } else {
+                    splitNumber.add("")
+                    splitText.add(splitContent[splitIdx])
+                }
+            }
+
+            layout.removeAllViews()
+
+            for (layoutIdx in splitContent.indices) {
+                val layoutInflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                val containView = layoutInflater.inflate(R.layout.mypage_content, null)
+                layout.addView(containView)
+
+                mVContentView[layoutIdx] = containView as View
+
+                mTvContentNumber[layoutIdx] = mVContentView[layoutIdx]!!.findViewById(R.id.tv_number) as TextView
+                mTvContentText[layoutIdx] = mVContentView[layoutIdx]!!.findViewById(R.id.tv_text) as TextView
+
+                mTvContentNumber[layoutIdx]!!.text = splitNumber[layoutIdx]
+                mTvContentText[layoutIdx]!!.text = splitText[layoutIdx]
+
+                if (TextUtils.isEmpty(splitNumber[layoutIdx])) {
+                    mTvContentNumber[layoutIdx]!!.visibility = View.GONE
+                }
+            }
+
+        } else {
+            // TODO: get your code!
+            Log.e("ERROR!", "Content is empty!");
+        }
     }
 
 }
