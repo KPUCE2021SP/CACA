5e9c56636bd911cc9f82fd8749a3dd4943f8e90b
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-02 22:14:12 +0900

    dynamic link + 매개변수

--- MyApplication/app/src/main/java/com/example/myapplication/DynamicLinkActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/DynamicLinkActivity.kt
@@ -1,25 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_dynamic.*
 
 class DynamicLinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dynamic)
 
+        var key = "a"
 
-
-        val dynamicLink = Firebase.dynamicLinks.dynamicLink {
-            link = Uri.parse("https://familyship.page.link/DynamicLinkActivity")
-            domainUriPrefix = "https://familyship.page.link/DynamicLinkActivity"
-            // Open links with this app on Android
-            androidParameters { }
+        if (intent.action == Intent.ACTION_VIEW) { // 앱 링크는 해당 인텐트 필터로만 가능하기 때문에
+            val value1 = intent.data?.getQueryParameter("key")
+            dynamicTextView.setText(value1)
         }
 
-        val dynamicLinkUri = dynamicLink.uri
+
     }
 
 }
