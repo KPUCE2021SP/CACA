4cd3bd980c424ed836a9bb11d71540ce7f16cc95
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-02 21:11:50 +0900

    dynamic link commit!

--- MyApplication/app/src/main/java/com/example/myapplication/DynamicLinkActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/DynamicLinkActivity.kt
@@ -1,12 +1,25 @@
 package com.example.myapplication
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
 
 class DynamicLinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dynamic)
+
+
+
+        val dynamicLink = Firebase.dynamicLinks.dynamicLink {
+            link = Uri.parse("https://familyship.page.link/DynamicLinkActivity")
+            domainUriPrefix = "https://familyship.page.link/DynamicLinkActivity"
+            // Open links with this app on Android
+            androidParameters { }
+        }
+
+        val dynamicLinkUri = dynamicLink.uri
     }
 
 }
--- MyApplication/app/src/main/java/com/example/myapplication/dialogActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/dialogActivity.kt
@@ -10,14 +10,15 @@ import androidx.core.app.ActivityCompat
 import java.text.SimpleDateFormat
 import java.util.*
 import android.Manifest
+import com.example.myapplication.R
 
-class MainActivity : AppCompatActivity() {
+class dialogActivity : AppCompatActivity() {
     lateinit var btnCall : Button
     lateinit var edtCall : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dialog)
         ActivityCompat.requestPermissions(this,
             arrayOf(Manifest.permission.READ_CALL_LOG), MODE_PRIVATE
         )
