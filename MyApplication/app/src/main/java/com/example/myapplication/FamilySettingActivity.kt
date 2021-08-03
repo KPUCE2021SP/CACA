fa2bdef2d4eb3f8585afa59d27b1af44c60efe61
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-03 13:41:43 +0900

    gallery accpet

--- MyApplication/app/src/main/java/com/example/myapplication/FamilySettingActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/FamilySettingActivity.kt
@@ -1,11 +1,53 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_familysetting.*
 
 class FamilySettingActivity : AppCompatActivity() {
+    private val OPEN_GALLERY = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_familysetting)
+
+        FamilyImageUpload.setOnClickListener(){ // 버튼 누르면 갤러리 접근
+            openGallery()
+        }
+
+
+    }
+
+    private fun openGallery(){ // intent로 갤러리 앱 접근
+        val intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        startActivityForResult(intent, OPEN_GALLERY)
+
+    }
+
+}
+
+
+@Override
+override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?){
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if(resultCode == Activity.RESULT_OK){
+        if(requestCode == OPEN_GALLERY){
+            var currentImageUrl : Url? = data?.data
+
+            try{
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, currentImageUrl)
+                FamilyImageView.setImageBitmap(bitmap)
+            }catch(e:Exception){
+                e.printStackTrace()
+            }
+        }else{
+            Log.d("FamilyImageView", "something wrong")
+        }
     }
 }
