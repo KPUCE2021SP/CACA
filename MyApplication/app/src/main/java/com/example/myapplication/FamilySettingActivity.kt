d39799b44753b4ca920aef40cea2a9340aeb4765
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-05 03:23:28 +0900

    Firestore Storage

--- MyApplication/app/src/main/java/com/example/myapplication/FamilySettingActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/FamilySettingActivity.kt
@@ -6,6 +6,7 @@ import android.app.AlertDialog
 import android.content.ContentUris
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
@@ -14,6 +15,10 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.net.toUri
+import androidx.core.view.drawToBitmap
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -21,6 +26,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_familysetting.*
+import java.io.File
 
 class FamilySettingActivity : AppCompatActivity() {
     private val OPEN_GALLERY = 1
@@ -41,16 +47,7 @@ class FamilySettingActivity : AppCompatActivity() {
             openGallery()
         }
 
-
-
-//        binding = ActivityStorageBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        Firebase.auth.currentUser ?: finish() // if not authenticated, finish this activity
         storage = Firebase.storage
-
-        button.setOnClickListener(){
-            uploadDialog()
-        }
 
     }
 
@@ -75,6 +72,24 @@ class FamilySettingActivity : AppCompatActivity() {
                 try{
                     val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, currentImageUrl)
                     FamilyImageView.setImageBitmap(bitmap)
+
+                    val path = intent.data?.path
+                    var file = Uri.fromFile(File(path))
+                    val storageRef = storage.reference
+                    val riversRef = storageRef.child("images/${file.lastPathSegment}")
+                    var uploadTask = riversRef.putFile(file)
+
+// Register observers to listen for when the download is done or if it fails
+                    uploadTask.addOnFailureListener {
+                        // Handle unsuccessful uploads
+                    }.addOnSuccessListener { taskSnapshot ->
+                        // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                        // ...
+                    }
+
+
+
+
                 }catch(e:Exception){
                     e.printStackTrace()
                 }
@@ -85,75 +100,5 @@ class FamilySettingActivity : AppCompatActivity() {
     }
 
 
-
-
-
-
-
-    private fun uploadDialog() {
-        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-            == PackageManager.PERMISSION_GRANTED) {
-            val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null, null, null, null)
-
-            AlertDialog.Builder(this)
-                .setTitle("Choose Photo")
-                .setCursor(cursor, { _, i ->
-                    cursor?.moveToPosition(i)
-                    uploadFile(cursor?.getLong(cursor.getColumnIndex(MediaStore.Images.ImageColumns._ID)),
-                        cursor?.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DISPLAY_NAME)))
-                }, MediaStore.Images.ImageColumns.DISPLAY_NAME).create().show()
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE)
-        }
-    }
-
-    private fun listPhotosDialog() {
-        storage.reference.child(UPLOAD_FOLDER).listAll()
-            .addOnSuccessListener {
-                val itemsString = mutableListOf<String>()
-                for (i in it.items) {
-                    itemsString.add(i.name)
-                }
-                AlertDialog.Builder(this)
-                    .setTitle("Uploaded Photos")
-                    .setItems(itemsString.toTypedArray(), {_, i -> }).show()
-            }.addOnFailureListener {
-
-            }
-    }
-
-    private fun uploadFile(file_id: Long?, fileName: String?) {
-        file_id ?: return
-        val imageRef : StorageReference? = storage.reference.child("${UPLOAD_FOLDER}${fileName}")
-        val contentUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, file_id)
-        imageRef?.putFile(contentUri)?.addOnCompleteListener {
-            if (it.isSuccessful) {
-                // upload success
-                var t1 = Toast.makeText(this, "Upload completed.", Toast.LENGTH_SHORT)
-                t1.show()
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE) {
-            if ((grantResults.isNotEmpty() &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                uploadDialog()
-            }
-        }
-    }
-
-    private fun displayImageRef(imageRef: StorageReference?, view: ImageView) {
-        imageRef?.getBytes(Long.MAX_VALUE)?.addOnSuccessListener {
-            val bmp = BitmapFactory.decodeByteArray(it, 0, it.size)
-            view.setImageBitmap(bmp)
-        }?.addOnFailureListener {
-            // Failed to download the image
-        }
-    }
 
 }
