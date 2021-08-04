27dece8559004eee2c9b085c9b018214ddd125a7
Jimin731 <petitejini@naver.com>
2021-08-04 13:20:51 +0900

    insert invite code xml

---
+++ MyApplication/app/src/main/java/com/example/myapplication/InsertInviteCode.kt
@@ -0,0 +1,11 @@
+package com.example.caca_xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InsertInviteCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert_invite_code)
+    }
+}
