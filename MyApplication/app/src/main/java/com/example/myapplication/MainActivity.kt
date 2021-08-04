6bcdbc345916dcfd43f2455aa449f8bda83ee30e
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-04 21:26:29 +0900

    Dynamic xml with DB

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.common.collect.ComparisonChain.start
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -23,9 +24,6 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var recyclerView: RecyclerView
-//    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-//    private lateinit var viewManager: RecyclerView.LayoutManager
 
     private var isFabOpen = false
 
@@ -39,23 +37,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // click animation
-        fabMain.setOnClickListener {
-            toggleFab()
-        }
-
-        // click event
-        fabCreate.setOnClickListener {
-            Toast.makeText(this,"가족생성 버튼 클릭", Toast.LENGTH_SHORT).show()
-//            val intent= Intent(this, DynamicLinkActivity::class.java)
-//            startActivity(intent)
-        }
-
-        fabInsert.setOnClickListener {
-            Toast.makeText(this,"초대코드 입력 버튼 클릭", Toast.LENGTH_SHORT).show()
-//            val intent= Intent(this, InsertInviteCode::class.java)
-//            startActivity(intent)
-        }
+//        // click animation
+//        fabMain.setOnClickListener {
+//            toggleFab()
+//        }
+//
+//        // click event
+//        fabCreate.setOnClickListener {
+//            Toast.makeText(this,"가족생성 버튼 클릭", Toast.LENGTH_SHORT).show()
+////            val intent= Intent(this, DynamicLinkActivity::class.java)
+////            startActivity(intent)
+//        }
+//
+//        fabInsert.setOnClickListener {
+//            Toast.makeText(this,"초대코드 입력 버튼 클릭", Toast.LENGTH_SHORT).show()
+////            val intent= Intent(this, InsertInviteCode::class.java)
+////            startActivity(intent)
+//        }
 
 
         val db: FirebaseFirestore = Firebase.firestore // 여러 document 받아오기
@@ -66,7 +64,7 @@ class MainActivity : AppCompatActivity() {
             .addOnSuccessListener { documents ->
                 for (document in documents) {
                     Log.d(TAG, "${document.id} => ${document.data}")
-                    a += document.id + ","
+                    //a += document.id + ","
                     val layoutInflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater // 동적으로 생성
 
                     val containView = layoutInflater.inflate(R.layout.card_layout, null) // mypage_content를 inflate // card_layout을 inflate
@@ -88,24 +86,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun toggleFab() {
-//        Toast.makeText(this, "메인 플로팅 버튼 클릭 : $isFabOpen", Toast.LENGTH_SHORT).show()
-
-        // 플로팅 액션 버튼 닫기 - 열려있는 플로팅 버튼 집어넣는 애니메이션 세팅
-        if (isFabOpen) {
-            ObjectAnimator.ofFloat(fabCreate, "translationY", 0f).apply { start() }
-            ObjectAnimator.ofFloat(fabInsert, "translationY", 0f).apply { start() }
-            fabMain.setImageResource(R.drawable.ic_baseline_add_24)
-
-            // 플로팅 액션 버튼 열기 - 닫혀있는 플로팅 버튼 꺼내는 애니메이션 세팅
-        } else {
-            ObjectAnimator.ofFloat(fabCreate, "translationY", -200f).apply { start() }
-            ObjectAnimator.ofFloat(fabInsert, "translationY", -400f).apply { start() }
-            fabMain.setImageResource(R.drawable.ic_baseline_clear_24)
-        }
-
-        isFabOpen = !isFabOpen
-
-    }
+//    private fun toggleFab() {
+////        Toast.makeText(this, "메인 플로팅 버튼 클릭 : $isFabOpen", Toast.LENGTH_SHORT).show()
+//
+//        // 플로팅 액션 버튼 닫기 - 열려있는 플로팅 버튼 집어넣는 애니메이션 세팅
+//        if (isFabOpen) {
+//            ObjectAnimator.ofFloat(fabCreate, "translationY", 0f).apply { start() }
+//            ObjectAnimator.ofFloat(fabInsert, "translationY", 0f).apply { start() }
+//            fabMain.setImageResource(R.drawable.ic_baseline_add_24)
+//
+//            // 플로팅 액션 버튼 열기 - 닫혀있는 플로팅 버튼 꺼내는 애니메이션 세팅
+//        } else {
+//            ObjectAnimator.ofFloat(fabCreate, "translationY", -200f).apply { start() }
+//            ObjectAnimator.ofFloat(fabInsert, "translationY", -400f).apply { start() }
+//            fabMain.setImageResource(R.drawable.ic_baseline_clear_24)
+//        }
+//
+//        isFabOpen = !isFabOpen
+//
+//    }
 
 }
