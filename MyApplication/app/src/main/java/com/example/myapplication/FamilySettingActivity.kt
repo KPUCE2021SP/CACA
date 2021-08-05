96739ae422bc5adb5ed0a812826e979a91c9990d
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-05 15:53:16 +0900

    upload Photo from Gallery && FireStorage COMPLETE!!!|'0'/

--- MyApplication/app/src/main/java/com/example/myapplication/FamilySettingActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/FamilySettingActivity.kt
@@ -1,66 +1,89 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.ContentUris
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_familysetting.*
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.util.Log
-import android.widget.Toast
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
+import java.util.jar.Manifest
 
 
 class FamilySettingActivity : AppCompatActivity() {
-    private val OPEN_GALLERY = 1
+    private var imagePreview: ImageView? = null
+    private val PICK_IMAGE_REQUEST = 1
+    private var filePath: Uri? = null
+    private var firebaseStorage: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_familysetting)
-
-        FamilyImageView.setOnClickListener {
-            openGallery()
-        }
+        firebaseStorage = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
+        FamilyImageView.setOnClickListener { ImagePicker() }
+        button.setOnClickListener { uploadImage() }
     }
 
-
-
-    private fun openGallery(){ // intent로 갤러리 앱 접근
-        val intent : Intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.setType("image/*")
-        startActivityForResult(intent, OPEN_GALLERY)
-
+    private fun ImagePicker() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode == OPEN_GALLERY){
-                var currentImageUrl = data?.data
-
-                try{
-                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, currentImageUrl)
-                    FamilyImageView.setImageBitmap(bitmap) // imageView에 표시
-
-                }catch (e: Exception){
-                    e.printStackTrace()
-                }
-            }else{
-                Log.d("FamilyImageView", "something wrong")
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if(data == null || data.data == null){
+                return
+            }
+            filePath = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                FamilyImageView?.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
         }
     }
 
+    private fun uploadImage(){
+        if(filePath != null){
+            val ref = storageReference?.child("uploads/" + "fileName_fileName")
+            ref?.putFile(filePath!!)?.addOnSuccessListener(OnSuccessListener<UploadTask.TaskSnapshot> {
+                Toast.makeText(applicationContext, "Image Uploaded", Toast.LENGTH_SHORT).show()
+            })?.addOnFailureListener(OnFailureListener { e ->
+                Toast.makeText(applicationContext, "Image Uploading Failed " + e.message, Toast.LENGTH_SHORT).show()
+            })
+        }else{
+            Toast.makeText(this, "Please Select an Image", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
