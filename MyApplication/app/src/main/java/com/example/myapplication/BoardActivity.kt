2d12607cfe93c1844e2e6281f5e3b219dbc967a8
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-06 20:03:29 +0900

    BoardActivity Plus

---
+++ MyApplication/app/src/main/java/com/example/myapplication/BoardActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.app.TabActivity
+import android.os.Bundle
+
+//edit_board_content
+
+class BoardActivity : TabActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.board)
+
+
+
+    }
+}
