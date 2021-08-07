8eef8a619d0ccc024c29844270461056a44ff366
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-07 12:17:14 +0900

    FCM setting

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 //import android.support.v7.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -213,59 +214,16 @@ class LoginActivity : AppCompatActivity() {
         val PASSWORD_PATTERN =
             Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{4,16}$")
     }
-}
 
 
 
 
-
-
-
-//import android.content.Intent
-//import android.graphics.BitmapFactory
-//import android.os.Bundle
-//import android.os.PersistableBundle
-//import android.widget.Toast
-//import androidx.appcompat.app.AppCompatActivity
-//import com.google.firebase.auth.ktx.auth
-//import com.google.firebase.ktx.Firebase
-//import com.google.firebase.storage.ktx.storage
-//import kotlinx.android.synthetic.main.loginactivity.*
+//    override fun onNewToken(token: String) { // FCM
+//        Log.d(TAG, "Refreshed token: $token")
 //
-//class LoginActivity :AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        //setContentView(null)
-//        setContentView(R.layout.loginactivity)
-//
-//
-//        SignUp.setOnClickListener {
-//            val userEmail = editID.text.toString()
-//            val password = editpw.text.toString()
-//            Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
-//                .addOnCompleteListener(this){
-//                    if(it.isSuccessful){
-//                        startActivity(Intent(this, MainActivity::class.java))
-//                    } else{
-//                        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//        }
-//        SignIn.setOnClickListener {
-//            val userEmail = editID.text.toString()
-//            val password = editpw.text.toString()
-//            Firebase.auth.signInWithEmailAndPassword(userEmail, password)
-//                .addOnCompleteListener(this){
-//                    if(it.isSuccessful){
-//                        startActivity(Intent(this, MainActivity::class.java))
-//                    } else{
-//                        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//
-//        }
-//
-//
-//
+//        // If you want to send messages to this application instance or
+//        // manage this apps subscriptions on the server side, send the
+//        // FCM registration token to your app server.
+//        sendRegistrationToServer(token)
 //    }
-//}
+}
