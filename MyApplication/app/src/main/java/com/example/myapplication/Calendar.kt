3ff1473982bf122b46ee0fa3ca42535c1c84b057
pavi03@kpu.ac.kr <pavi03@kpu.ac.kr>
2021-08-14 04:51:11 +0900

    Calendar Design

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Calendar.kt
@@ -0,0 +1,215 @@
+//package com.example.myapplication
+//
+//import android.os.Bundle
+//import android.util.TypedValue
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.annotation.ColorRes
+//import androidx.appcompat.widget.Toolbar
+//import androidx.core.view.children
+//import androidx.recyclerview.widget.DividerItemDecoration
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
+//import com.kizitonwose.calendarview.model.CalendarDay
+//import com.kizitonwose.calendarview.model.CalendarMonth
+//import com.kizitonwose.calendarview.model.DayOwner
+//import com.kizitonwose.calendarview.ui.DayBinder
+//import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
+//import com.kizitonwose.calendarview.ui.ViewContainer
+//import kotlinx.android.extensions.LayoutContainer
+//import kotlinx.android.synthetic.main.calendar_day_legend.view.*
+//import kotlinx.android.synthetic.main.example_5_calendar_day.view.*
+//import kotlinx.android.synthetic.main.example_5_event_item_view.*
+//import kotlinx.android.synthetic.main.exmaple_5_fragment.*
+//import org.threeten.bp.LocalDate
+//import org.threeten.bp.LocalDateTime
+//import org.threeten.bp.YearMonth
+//import org.threeten.bp.format.DateTimeFormatter
+//import java.time.LocalDateTime
+//import java.time.format.DateTimeFormatter
+//
+//data class Flight(val time: LocalDateTime, val departure: Airport, val destination: Airport, @ColorRes val color: Int) {
+//    data class Airport(val city: String, val code: String)
+//}
+//
+//class Example5FlightsAdapter : RecyclerView.Adapter<Example5FlightsAdapter.Example5FlightsViewHolder>() {
+//
+//    val flights = mutableListOf<Flight>()
+//
+//    private val formatter = DateTimeFormatter.ofPattern("EEE'\n'dd MMM'\n'HH:mm")
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Example5FlightsViewHolder {
+//        return Example5FlightsViewHolder(parent.inflate(R.layout.activity_calendar_view))
+//    }
+//
+//    override fun onBindViewHolder(viewHolder: Example5FlightsViewHolder, position: Int) {
+//        viewHolder.bind(flights[position])
+//    }
+//
+//    override fun getItemCount(): Int = flights.size
+//
+//    inner class Example5FlightsViewHolder(override val containerView: View) :
+//        RecyclerView.ViewHolder(containerView), LayoutContainer {
+//
+//        fun bind(flight: Flight) {
+//            itemFlightDateText.text = formatter.format(flight.time)
+//            itemFlightDateText.setBackgroundColor(itemView.context.getColorCompat(flight.color))
+//
+//            itemDepartureAirportCodeText.text = flight.departure.code
+//            itemDepartureAirportCityText.text = flight.departure.city
+//
+//            itemDestinationAirportCodeText.text = flight.destination.code
+//            itemDestinationAirportCityText.text = flight.destination.city
+//        }
+//    }
+//}
+//
+//
+//class Calendar : BaseFragment(), HasToolbar {
+//
+//    override val toolbar: Toolbar?
+//        get() = null
+//
+//    override val titleRes: Int = R.string.example_5_title
+//
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        return inflater.inflate(R.layout.exmaple_5_fragment, container, false)
+//    }
+//
+//    private var selectedDate: LocalDate? = null
+//    private val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
+//
+//    private val flightsAdapter = Example5FlightsAdapter()
+//    private val flights = generateFlights().groupBy { it.time.toLocalDate() }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        exFiveRv.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+//        exFiveRv.adapter = flightsAdapter
+//        exFiveRv.addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+//        flightsAdapter.notifyDataSetChanged()
+//
+//        val daysOfWeek = daysOfWeekFromLocale()
+//
+//        val currentMonth = YearMonth.now()
+//        exFiveCalendar.setup(currentMonth.minusMonths(10), currentMonth.plusMonths(10), daysOfWeek.first())
+//        exFiveCalendar.scrollToMonth(currentMonth)
+//
+//        class DayViewContainer(view: View) : ViewContainer(view) {
+//            lateinit var day: CalendarDay // Will be set when this container is bound.
+//            val textView = view.exFiveDayText
+//            val layout = view.exFiveDayLayout
+//            val flightTopView = view.exFiveDayFlightTop
+//            val flightBottomView = view.exFiveDayFlightBottom
+//
+//            init {
+//                view.setOnClickListener {
+//                    if (day.owner == DayOwner.THIS_MONTH) {
+//                        if (selectedDate != day.date) {
+//                            val oldDate = selectedDate
+//                            selectedDate = day.date
+//                            exFiveCalendar.notifyDateChanged(day.date)
+//                            oldDate?.let { exFiveCalendar.notifyDateChanged(it) }
+//                            updateAdapterForDate(day.date)
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        exFiveCalendar.dayBinder = object : DayBinder<DayViewContainer> {
+//            override fun create(view: View) = DayViewContainer(view)
+//            override fun bind(container: DayViewContainer, day: CalendarDay) {
+//                container.day = day
+//                val textView = container.textView
+//                val layout = container.layout
+//                textView.text = day.date.dayOfMonth.toString()
+//
+//                val flightTopView = container.flightTopView
+//                val flightBottomView = container.flightBottomView
+//
+//                flightTopView.background = null
+//                flightBottomView.background = null
+//
+//                if (day.owner == DayOwner.THIS_MONTH) {
+//                    textView.setTextColorRes(R.color.example_5_text_grey)
+//                    layout.setBackgroundResource(if (selectedDate == day.date) R.drawable.example_5_selected_bg else 0)
+//
+//                    val flights = flights[day.date]
+//                    if (flights != null) {
+//                        if (flights.count() == 1) {
+//                            flightBottomView.setBackgroundColor(view.context.getColorCompat(flights[0].color))
+//                        } else {
+//                            flightTopView.setBackgroundColor(view.context.getColorCompat(flights[0].color))
+//                            flightBottomView.setBackgroundColor(view.context.getColorCompat(flights[1].color))
+//                        }
+//                    }
+//                } else {
+//                    textView.setTextColorRes(R.color.example_5_text_grey_light)
+//                    layout.background = null
+//                }
+//            }
+//        }
+//
+//        class MonthViewContainer(view: View) : ViewContainer(view) {
+//            val legendLayout = view.legendLayout
+//        }
+//        exFiveCalendar.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+//            override fun create(view: View) = MonthViewContainer(view)
+//            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+//                // Setup each header day text if we have not done that already.
+//                if (container.legendLayout.tag == null) {
+//                    container.legendLayout.tag = month.yearMonth
+//                    container.legendLayout.children.map { it as TextView }.forEachIndexed { index, tv ->
+//                        tv.text = daysOfWeek[index].name.take(3)
+//                        tv.setTextColorRes(R.color.example_5_text_grey)
+//                        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
+//                    }
+//                }
+//            }
+//        }
+//
+//        exFiveCalendar.monthScrollListener = { month ->
+//            val title = "${monthTitleFormatter.format(month.yearMonth)} ${month.yearMonth.year}"
+//            exFiveMonthYearText.text = title
+//
+//            selectedDate?.let {
+//                // Clear selection if we scroll to a new month.
+//                selectedDate = null
+//                exFiveCalendar.notifyDateChanged(it)
+//                updateAdapterForDate(null)
+//            }
+//        }
+//
+//        exFiveNextMonthImage.setOnClickListener {
+//            exFiveCalendar.getFirstVisibleMonth()?.let {
+//                exFiveCalendar.smoothScrollToMonth(it.next.yearMonth)
+//            }
+//        }
+//
+//        exFivePreviousMonthImage.setOnClickListener {
+//            exFiveCalendar.getFirstVisibleMonth()?.let {
+//                exFiveCalendar.smoothScrollToMonth(it.previous.yearMonth)
+//            }
+//        }
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//        requireActivity().window.statusBarColor = requireContext().getColorCompat(R.color.example_5_toolbar_color)
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        requireActivity().window.statusBarColor = requireContext().getColorCompat(R.color.colorPrimaryDark)
+//    }
+//
+//    private fun updateAdapterForDate(date: LocalDate?) {
+//        flightsAdapter.flights.clear()
+//        flightsAdapter.flights.addAll(flights[date].orEmpty())
+//        flightsAdapter.notifyDataSetChanged()
+//    }
+//
+//}
